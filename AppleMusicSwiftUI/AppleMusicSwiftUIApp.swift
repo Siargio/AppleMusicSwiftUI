6c6a852fc416025ef866a2fd2e6a169a87938553
Siarheimasiuk <winst73@gmail.com>
2022-11-25 12:01:14 +0300

    Initial Commit

---
+++ AppleMusicSwiftUI/AppleMusicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicSwiftUIApp.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Sergio on 25.11.22.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
